8442c6df47f7b15fe5dcf95b203505a689db8e27
mary-code217 <xorl0514@naver.com>
2026-02-22 18:44:50 +0900

    test: db-core 테스트 기반 클래스 Java 변환
    - CoreDbTestApplication (테스트용 Spring Boot 메인)
    - CoreDbContextTest (추상 테스트 베이스 - @ActiveProfiles, @Tag)

---
+++ storage/db-core/src/test/java/io/dodn/commerce/storage/db/CoreDbTestApplication.java
@@ -0,0 +1,14 @@
+package io.dodn.commerce.storage.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+public class CoreDbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreDbTestApplication.class, args);
+    }
+}
