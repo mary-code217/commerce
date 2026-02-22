c4a93410cc6ccb59a6061b138cfb13757124c701
mary-code217 <xorl0514@naver.com>
2026-02-22 18:45:19 +0900

    test: core-api 테스트 기반 클래스 Java 변환
    - ContextTest (추상 베이스 - @Tag context)
    - DevelopTest (추상 베이스 - @Tag develop)
    - CoreApiApplicationTest (컨텍스트 로드 확인)

---
+++ core/core-api/src/test/java/io/dodn/commerce/CoreApiApplicationTest.java
@@ -0,0 +1,11 @@
+package io.dodn.commerce;
+
+import org.junit.jupiter.api.Test;
+
+class CoreApiApplicationTest extends ContextTest {
+
+    @Test
+    void shouldBeLoadedContext() {
+        // it should be passed
+    }
+}
